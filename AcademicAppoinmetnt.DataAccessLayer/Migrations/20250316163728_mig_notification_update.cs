4412460b2da9a800c7b3313f4ac7079e94565937
Yunus Emre Maral <108576886+yunusemremaral@users.noreply.github.com>
2025-03-16 19:56:13 +0300

    Notification controller baglanması ve calıstırılması . Suanlık sayfa şeklinde fakat view componente baglanılacak .

---
+++ AcademicAppoinmetnt.DataAccessLayer/Migrations/20250316163728_mig_notification_update.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademicAppoinmetnt.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_notification_update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Notifications");
+        }
+    }
+}
--- AcademicAppoinmetnt.Presentation/Models/NotificationViewModel.cs
+++ AcademicAppoinmetnt.Presentation/Models/NotificationViewModel.cs
@@ -3,6 +3,7 @@
     public class NotificationViewModel
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string Message { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool IsRead { get; set; }
